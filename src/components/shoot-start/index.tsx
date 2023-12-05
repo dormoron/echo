762edd1453beddea1363c892388d63b8d4e1da88
dormoron <lzqhying@163.com>
2023-12-05 13:39:06 +0800

    修改首页图标

--- src/components/shoot-start/index.tsx
+++ src/components/shoot-start/index.tsx
@@ -1,59 +1,66 @@
-import './styles/index.scss'
 const ShootStart = () => {
     return (
-        <div class="m-container">
-            <div class="circle-container">
+        <div class="w-112.5 h-112.5 relative">
+            <div class="w-112.5 h-112.5 absolute overflow-hidden circle">
                 {/*天空部分*/}
-                <div class="sky">
-                    <div class="sky-night sky-night-fade">
-                        <div class="stars stars-fade">
-                            <div class="shooting-star shooting"></div>
-                            <div class="star-group-1"></div>
-                            <div class="star-group-2"></div>
-                            <div class="star-group-3"></div>
-                            <div class="star-group-4"></div>
-                        </div>
+                <div class="absolute w-full h-[55%] top-0 overflow-hidden">
+                    <div class="absolute w-full h-full bg-night">
+                        <div class="shooting-star bg-shooting absolute w-8.75 h-0.75 left-85 top-6.4"></div>
+                        <div class="w-1.2 h-1.2 left-55 top-30 stars animate-[twinkle_2s_infinite_linear] delay-500"></div>
+                        <div class='w-1 h-1 left-52 top-7 opacity-80 stars animate-[twinkle_2.3s_infinite_linear]'></div>
+                        <div class='w-1 h-1 left-43 top-17 opacity-100 stars animate-[twinkle_2.7s_infinite_linear]'></div>
+
+                        <div class="w-1.2 h-1.2 left-6 top-44 stars animate-[twinkle_1s_infinite_linear]"></div>
+                        <div class='w-1 h-1 left-15 top-45 opacity-80 stars animate-[twinkle_1.3s_infinite_linear]'></div>
+                        <div class='w-1 h-1 left-10 top-35 opacity-100 stars animate-[twinkle_1.7s_infinite_linear]'></div>
+
+                        <div class="w-1.2 h-1.2 left-31 top-13 stars animate-[twinkle_0.8s_infinite_linear]"></div>
+                        <div class='w-1 h-1 left-65 top-37 opacity-80 stars animate-[twinkle_1.1s_infinite_linear]'></div>
+                        <div class='w-1 h-1 left-47 top-40 opacity-100 stars animate-[twinkle_1.5s_infinite_linear]'></div>
+
+                        <div class="w-1.2 h-1.2 left-75 top-25 stars animate-[twinkle_1s_infinite_linear]"></div>
+                        <div class='w-1 h-1 left-85 top-20 opacity-80 stars animate-[twinkle_1.1s_infinite_linear]'></div>
+                        <div class='w-1 h-1 left-100 top-37 opacity-100 stars animate-[twinkle_1.5s_infinite_linear]'></div>
                     </div>
                 </div>
 
                 {/*山川*/}
-                <div class="mountains">
-                    <div class="mountain-back mountain-back-night">
-                        <div class="mountain-back-1 mountain-back-night"></div>
-                        <div class="mountain-back-2 mountain-back-night"></div>
+                <div class="absolute w-full h-[20%] bottom-[35%]">
+                    <div class="w-45 h-30 top-0 bg-distant mountains">
+                        <div class="w-50 h-25 top-0.75 m-l-20 bg-distant mountains"></div>
+                        <div class="w-37.5 h-20 m-l-62.5 bg-distant mountains"></div>
                     </div>
-                    <div class="mountain-middle mountain-middle-night">
-                        <div class="mountain-middle-1 mountain-middle-night"></div>
-                        <div class="mountain-middle-2 mountain-middle-night"></div>
+                    <div class="w-45 h-30 top-3 m-l-45 bg-mountain mountains">
+                        <div class="w-50 h-25 top-0.75 -m-l-27.5 bg-mountain mountains"></div>
+                        <div class="w-55 h-37.5 -m-l-62.5 bg-mountain mountains"></div>
                     </div>
-                    <div class="mountain-top mountain-top-night">
-                        <div class="mountain-top-1 mountain-top-night"></div>
+                    <div class="w-50 h-22.5 top-4.5 right-0 -m-r-12.5 bg-mountain mountains">
+                        <div class="absolute w-62.5 h-20 top-3.75 right-0 m-r-37.5 bg-mountain mountains"></div>
                     </div>
                 </div>
 
-                {/*太阳*/}
-                <div class="sun-container">
-                    <div class="sun"></div>
+                {/*月亮*/}
+                <div class="absolute w-72.5 h-72.5 m-auto inset-0 z-10">
+                    <div class="absolute w-22.5 h-22.5 rounded-[50%] bg-moon"></div>
                 </div>
 
                 {/*海洋*/}
-                <div class="ocean animation-stop">
-                    <div class="boat boat-sail"></div>
-                    <div class="ocean-night ocean-night-fade">
-                        <div class="moon moon-fade"></div>
+                <div class="absolute w-full h-[42%] bottom-0 z-200 opacity-100 transition-all duration-300 ease delay-200 origin-t animate-[ocean_3s_forwards] bg-ripple">
+                    <div class="relative w-full h-full bg-undersea opacity-100 transition-opacity-300 delay-200 z-500">
+                        <div class="absolute w-22.5 h-22.5 left-20 m-t-22.5 rounded-[50%] bg-bg-waterMoon"></div>
 
                         {/*山川*/}
-                        <div class="mountains mountains-bottom">
-                            <div class="mountain-back mountain-back-night">
-                                <div class="mountain-back-1 mountain-back-night"></div>
-                                <div class="mountain-back-2 mountain-back-night"></div>
+                        <div class="absolute w-full h-[20%] bottom-[35%] opacity-50 origin-c top-0 fixed overflow-hidden h-[31.5%] mountain-bottom">
+                            <div class="w-45 h-30 top-0 bg-distant mountains">
+                                <div class="w-50 h-25 top-0.75 m-l-20 bg-distant mountains"></div>
+                                <div class="w-37.5 h-20 m-l-62.5 bg-distant mountains"></div>
                             </div>
-                            <div class="mountain-middle mountain-middle-night">
-                                <div class="mountain-middle-1 mountain-middle-night"></div>
-                                <div class="mountain-middle-2 mountain-middle-night"></div>
+                            <div class="w-45 h-30 top-3 m-l-45 bg-mountain mountains">
+                                <div class="w-50 h-25 top-0.75 -m-l-27.5 bg-mountain mountains"></div>
+                                <div class="w-55 h-37.5 -m-l-62.5 bg-mountain mountains"></div>
                             </div>
-                            <div class="mountain-top mountain-top-night">
-                                <div class="mountain-top-1 mountain-top-night"></div>
+                            <div class="w-50 h-22.5 top-4.5 right-0 -m-r-12.5 bg-mountain mountains">
+                                <div class="absolute w-62.5 h-20 top-3.75 right-0 m-r-37.5 bg-mountain mountains"></div>
                             </div>
                         </div>
                     </div>
--- uno.config.ts
+++ uno.config.ts
@@ -3,11 +3,11 @@ import { defineConfig, presetAttributify, presetTypography, presetUno } from 'un
 
 export default defineConfig({
   // ...UnoCSS options
-  presets:[
+  presets: [
     presetAttributify(),
     presetTypography({
-      cssExtend:{
-        'pre':{
+      cssExtend: {
+        'pre': {
           color: '#f43f5e',
           backageColor: '#282c34',
         }
@@ -15,21 +15,58 @@ export default defineConfig({
     }),
     presetUno()
   ],
-  theme:{
-    colors:{
-        "primary": '#1565D8',
-        "dark":{
-          "hard":'#0D2436',
-          "light":'#5A7184',
-          "soft": '#183B56'
-        }
-    },
-    clipPath: {
-      'circle-50': 'circle(50% at 50% 50%)',
+  theme: {
+    colors: {
+      "primary": '#1565D8',
+      "dark": {
+        "hard": '#0D2436',
+        "light": '#5A7184',
+        "soft": '#183B56'
+      },
+      waterMoon: "#A6B5DD",
+      ripple: "#ceefe8",
+      mountain: "#614da2",
+      distant: "#7980c8"
     },
     fontFamily: {
       opensans: ["'Open Sans'", "sans-serif"],
       roboto: ["'Roboto'", "sans-serif"],
     },
-  }
+  },
+  rules: [
+    ['stars', {
+      'border-radius': '50%',
+      'background-color': '#fff',
+      'position': 'absolute',
+    }],
+    ['circle', {
+      'clip-path': 'circle(50% at 50% 50%)'
+    }],
+    ['bg-night', {
+      'background': 'linear-gradient(#4c3f6d, #6c62bb, #93b1ed)'
+    }],
+    ['bg-undersea', {
+      'background': 'linear-gradient(#8da6e6, #788cc9)'
+    }],
+    ['bg-moon', {
+      'background': 'linear-gradient(#fefefe, #fffbe8)'
+    }],
+    ['bg-shooting', {
+      'background': 'linear-gradient(to right,rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 0) 100%)'
+    }],
+    ['shooting-star', {
+      'animation': 'shooting-star 2s 2 ease-in-out',
+      'animation-delay': '5s',
+      'transform-origin': 'left',
+      'transform': 'rotate(-40deg)'
+    }],
+    ['mountain-bottom', {
+      'transform': 'scaleY(-1)'
+    }],
+    ['mountains', {
+      'border-radius': '50%',
+      'position': 'absolute',
+      'z-index': '200',
+    }]
+  ]
 })
